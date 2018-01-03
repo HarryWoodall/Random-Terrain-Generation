85eb918b543e4c58c8ebad3f4ff3b0725f39181f
Harry <h.woodall311@hotmail.co.uk>
2018-01-03 00:12:43 +0000

    Initial Commit

---
+++ MapGenerator/MapGenerator/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace MapGenerator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.mainContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // mainContainer
+            // 
+            this.mainContainer.Location = new System.Drawing.Point(102, 44);
+            this.mainContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.mainContainer.Name = "mainContainer";
+            this.mainContainer.Size = new System.Drawing.Size(900, 500);
+            this.mainContainer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 589);
+            this.Controls.Add(this.mainContainer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel mainContainer;
+    }
+}
+
---
+++ MapGenerator/MapGenerator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapGenerator {
+    public partial class Form1 : Form {
+
+        Panel[,] map;
+        int pixelSize = 20;
+        int mapX;
+        int mapY;
+        Random random = new Random();
+
+        public Form1() {
+            InitializeComponent();
+            mapX = mainContainer.Width / pixelSize;
+            mapY = mainContainer.Height / pixelSize;
+            map = new Panel[mapX, mapY];
+            createMap();
+            Color color = populateMap(populateMap(populateMap(Color.Green)));
+            addNoise();
+        }
+
+        private void createMap() {
+            for (int i = 0; i < mapY; i++) {
+                for (int j = 0; j < mapX; j++) {
+                    Panel panel = new Panel();
+                    panel.Padding = new Padding(0);
+                    panel.Margin = new Padding(0);
+                    panel.Size = new Size(pixelSize, pixelSize);
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    panel.BackColor = Color.White;
+                    mainContainer.Controls.Add(panel);
+                    map[j, i] = panel;
+                    if (random.Next(1, 30) > 28) {
+                        map[j, i].BackColor = Color.Green;
+                    }
+                }
+            }
+        }
+
+        private Color populateMap(Color color) {
+            for (int i = 0; i < mapY; i++) {
+                for (int j = 0; j < mapX; j++) {
+                    if (map[j,i].BackColor == color) {
+
+                        if (j !=0) {
+                            map[j - 1, i].BackColor = Color.FromArgb(color.A - 30, color);
+                        }
+                        if (j != mapX - 1) {
+                            map[j + 1, i].BackColor = Color.FromArgb(color.A - 30, color);
+                        }
+
+                        if (i != 0) {
+                            map[j, i - 1].BackColor = Color.FromArgb(color.A - 30, color);
+                        }
+                        if (i != mapY - 1) {
+                            map[j, i + 1].BackColor = Color.FromArgb(color.A - 30, color);
+                        }
+                    }
+                }
+            }
+            return Color.FromArgb(color.A - 30, color);
+        }
+
+        private void addNoise() {
+            for (int i = 0; i < mapY; i++) {
+                for (int j = 0; j < mapX; j++) {
+                    int counter = 0;
+                    
+                    if (i != mapY - 1 && map[j, i + 1].BackColor != Color.White) {
+                        counter++;
+                    }
+
+                    if (i != 0 && map[j, i - 1].BackColor != Color.White) {
+                        counter++;
+                    }
+
+                    if (j != mapX - 1 &&map[j + 1, i].BackColor != Color.White) {
+                        counter++;
+                    }
+
+                    if (j != 0 && map[j - 1, i].BackColor != Color.White) {
+                        counter++;
+                    }
+
+                    if (counter >= 3) {
+                        map[j, i].BackColor = Color.Green;
+                    }
+                }
+            }
+        }
+    }
+}
